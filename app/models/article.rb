16fc9bbaabf4b21098ad19fcf4de5a01873b92b3
RostykRadzhapov <rostykradzhapov@gmail.com>
2020-09-09 16:25:30 +0300

    Creating ui design for application

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,17 +6,12 @@ class Article < ApplicationRecord
 
   has_many_attached :images
   has_many_attached :videos
-  validate :images_presence, :images_type
-  validate :videos_presence, :videos_type
+  validate :images_type, :videos_type
 
   private
 
   def generate_reference
     GenerateReferenceService.new.call
-  end
-
-  def images_presence
-    errors.add(:images, 'is missing') unless images.attached?
   end
 
   def images_type
@@ -25,10 +20,6 @@ class Article < ApplicationRecord
 
       errors.add(:images, 'needs to be JPEG or PNG')
     end
-  end
-
-  def videos_presence
-    errors.add(:videos, 'is missing') unless videos.attached?
   end
 
   def videos_type
